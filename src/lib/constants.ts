a23032de34e79482fe013278c49d9dd90e189a21
offcwho <nhnhnhbgbg@gmail.com>
2025-11-23 23:30:46 +0300

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL: string = "https://configure-backend-production.up.railway.app/api";
 export const ACCESS_TOKEN: string = process.env.JWT_ACCESS_TOKEN || "";
-
-export const BACKEND_HEADERS = { 'Content-Type' : 'application/json' };
+//application/json
+export const BACKEND_HEADERS = { 'Content-Type': '' };
