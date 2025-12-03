d59a4a4fffcdfb7396e5b703f36df391e12b473e
offcwho <nhnhnhbgbg@gmail.com>
2025-12-04 02:09:11 +0300

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,3 @@
-import "dotenv/config"
-
 export const BACKEND_URL: string = process.env.NEXT_PUBLIC_API_URL || "http://localhost:9000/api";
 export const BACKEND_IMAGE: string = process.env.NEXT_PUBLIC_IMAGES || "https://localhost:9000/";
 export const ACCESS_TOKEN: string = process.env.JWT_ACCESS_TOKEN || "";
