936d723a7d0bcbc4132d6caaa592e9ec2970afba
offcwho <nhnhnhbgbg@gmail.com>
2025-12-04 01:44:50 +0300

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,8 @@
-export const BACKEND_URL: string = "https://configure-backend-production.up.railway.app/api";
+import "dotenv/config"
+
+export const BACKEND_URL: string = process.env.NEXT_PUBLIC_API_URL || "http://localhost:9000/api";
+export const BACKEND_IMAGE: string = process.env.NEXT_PUBLIC_IMAGES || "https://localhost:9000/";
 export const ACCESS_TOKEN: string = process.env.JWT_ACCESS_TOKEN || "";
 
 export const BACKEND_HEADERS = { 'Content-Type': 'application/json' };
 export const BACKEND_HEADERS_FILE = { 'Content-Type': '' };
-export const BACKEND_IMAGE = "https://configure-backend-production.up.railway.app/";
